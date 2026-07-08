227850eb1c34db4de3c0c2782bba388a7198320d
David <dmko2002@gmail.com>
2026-07-08 19:59:17 -0300

    fix: disable Vercel image optimization (unoptimized: true)

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ const nextConfig = {
     return [{ source: '/(.*)', headers: securityHeaders }]
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/v1/object/public/**' },
     ],
